96e1531762d63cb828b458e88b7c72c22f2bb1ce
DavidBakalov21 <davidbakalov33@gmail.com>
2026-05-03 17:20:15 +0300

    f

--- backend-ramio/docker-entrypoint.sh
+++ backend-ramio/docker-entrypoint.sh
@@ -5,7 +5,7 @@ mkdir -p "$WORKDIR"
 chown -R 1000:1000 "$WORKDIR"
 
 DOCKER_GID="${DOCKER_GID:-988}"
-# Drop from image default (root) to app UID; supplementary GIDs must match host
-# `docker` group so /var/run/docker.sock is usable (DooD). Use --groups (portable);
-# --supp-group is not available on Debian bookworm / Ubuntu setpriv.
-exec setpriv --reuid=1000 --regid=1000 --clear-groups --groups "$DOCKER_GID" --inh-caps=-all -- "$@"
+# Drop from image default (root) to app UID; supplementary GID must match host
+# `docker` group so /var/run/docker.sock is usable (DooD). Do not combine --groups
+# with --clear-groups (mutually exclusive on setpriv).
+exec setpriv --reuid=1000 --regid=1000 --groups "$DOCKER_GID" --inh-caps=-all -- "$@"
